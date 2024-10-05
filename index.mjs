8412a0b6eb218d8d98a0c10b9815d690474e0674
kcaparas1630 <kcaparas1630@gmail.com>
2024-10-05 11:36:07 -0700

    readline imported, package.json type changed to commonjs

--- index.mjs
+++ index.mjs
@@ -1,12 +1,15 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
-    console.log('Example app listening to port 3000');
-})
+  rl.question("What is your age? ", (age) => {
+    console.log("Your age is: " + age);
+  });
+  console.log("Example app listening to port 3000");
+});
---
+++ readline.js
@@ -0,0 +1,8 @@
+const readLine = require('readline');
+const { stdin: input, stdout: output } = require('node:process');
+const rl = readLine.createInterface({ input, output });
+
+rl.question(`What's your age?: `, (input) => {
+    console.log(`Your age is ${input}`);
+    rl.close();
+});
